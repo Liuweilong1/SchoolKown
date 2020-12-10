d6ea870815be46ad083a088e946350d923d4ade8
123456gaodan <2548055895@qq.com>
2020-12-10 10:10:52 +0800

    高丹上传

--- sealtalk/src/main/java/cn/rongcloud/im/ui/Parent/SmallActivity/SchoolActivity/ListDetailActivity2.java
+++ sealtalk/src/main/java/cn/rongcloud/im/ui/Parent/SmallActivity/SchoolActivity/ListDetailActivity2.java
@@ -3,14 +3,35 @@ package cn.rongcloud.im.ui.Parent.SmallActivity.SchoolActivity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import cn.rongcloud.im.R;
 
 public class ListDetailActivity2 extends AppCompatActivity {
 
+    private Button btn_open;
+    private boolean status=false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_detail2);
+
+        btn_open.setText("点击参与");
+        btn_open= (Button) findViewById(R.id.activity_btn_good2);
+
+    }
+
+    public void onClick(View view) {
+        if(!status){
+            btn_open.setText("已参与");
+            status=true;
+        }else{
+            btn_open.setText("点击参与");
+            status=false;
+        }
     }
 }
+
+
--- sealtalk/src/main/java/cn/rongcloud/im/ui/Parent/SmallActivity/SchoolActivity/ListDetailActivity3.java
+++ sealtalk/src/main/java/cn/rongcloud/im/ui/Parent/SmallActivity/SchoolActivity/ListDetailActivity3.java
@@ -3,14 +3,33 @@ package cn.rongcloud.im.ui.Parent.SmallActivity.SchoolActivity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import cn.rongcloud.im.R;
 
 public class ListDetailActivity3 extends AppCompatActivity {
 
+    private Button btn_open;
+    private boolean status=false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_detail3);
+
+        btn_open.setText("点击参与");
+        btn_open= (Button) findViewById(R.id.activity_btn_good3);
+
+    }
+
+    public void onClick(View view) {
+        if(!status){
+            btn_open.setText("已参与");
+            status=true;
+        }else{
+            btn_open.setText("点击参与");
+            status=false;
+        }
     }
 }
